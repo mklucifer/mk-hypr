e267e52ea6e1c1b1dd0d2efd4819804e913b4c81
Mk <mk_lucifer@hotmail.com>
2025-07-09 21:25:29 +0100

    neovim background transparent added

--- config/nvim/lua/vim-options.lua
+++ config/nvim/lua/vim-options.lua
@@ -4,5 +4,20 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set cursorline")
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.cmd [[
+      highlight Normal ctermbg=none guibg=none
+      highlight NormalNC ctermbg=none guibg=none
+      highlight EndOfBuffer ctermbg=none guibg=none
+      highlight SignColumn ctermbg=none guibg=none
+      highlight VertSplit ctermbg=none guibg=none
+    ]]
+  end
+})
+
 
 vim.g.mapleader = " "
