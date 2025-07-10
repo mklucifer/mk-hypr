d2377a98299f3a94fc2c56a912048b00b277c2db
Mk <mk_lucifer@hotmail.com>
2025-07-10 16:01:01 +0100

    nvim config alterations

--- config/nvim/lua/plugins/debugging.lua
+++ config/nvim/lua/plugins/debugging.lua
@@ -2,19 +2,20 @@ return {
   "rcarriga/nvim-dap-ui",
   dependencies = {
     "mfussenegger/nvim-dap",
-    "nvim-neotest/nvim-nio"
+    "mfussenegger/nvim-dap-python",
+    "nvim-neotest/nvim-nio",
   },
   config = function()
     require("dap-python").setup("debugpy-adapter") -- or python3 instead of debugpy-adapter
-    
+
     local dap, dapui = require("dap"), require("dapui")
 
     dap.adapters.gdb = {
       type = "executable",
       command = "gdb",
-      args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+      args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
     }
-    
+
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
     end
@@ -27,8 +28,8 @@ return {
     dap.listeners.before.event_exited.dapui_config = function()
       dapui.close()
     end
-    
-    vim.keymap.set('n', '<Leader>db', dap.toggle_breakpoint() {})
-    vim.keymap.set('n', '<Leader>dc', dap.continue() {})
-  end
+
+    vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint)
+    vim.keymap.set("n", "<Leader>dc", dap.continue)
+  end,
 }
