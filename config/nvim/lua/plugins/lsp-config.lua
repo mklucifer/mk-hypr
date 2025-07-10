79f9bcc94fbb4af5b31ac966088b7cdd053fe7bc
mklucifer <61414632+mklucifer@users.noreply.github.com>
2025-07-10 12:56:51 +0100

    Update lsp-config.lua

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -63,13 +63,6 @@ return {
 			lspconfig.pylyzer.setup({
 				capabilities = capabilities,
 			})
-
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 		end,
 	},
 }
