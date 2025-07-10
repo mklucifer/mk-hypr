1bc8d76fb799bae05154db675e59feef8661f0f8
mklucifer <61414632+mklucifer@users.noreply.github.com>
2025-07-10 14:33:27 +0100

    Update vim-options.lua

--- config/nvim/lua/vim-options.lua
+++ config/nvim/lua/vim-options.lua
@@ -5,6 +5,10 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set cursorline")
+vim.cmd("set ignorecase")
+vim.cmd("set smartcase")
+vim.cmd("set scrolloff=8")
+vim.cmd("set sidescrolloff=8")
 
 vim.api.nvim_create_autocmd("ColorScheme", {
   pattern = "*",
