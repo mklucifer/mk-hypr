c560dc3d4f775cf9818d7248159c7be912b1218b
mklucifer <61414632+mklucifer@users.noreply.github.com>
2025-07-10 12:57:59 +0100

    Update keymaps.lua

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -13,3 +13,7 @@ vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+
+-- Neotree
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle <CR>", {})
+
