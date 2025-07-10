ac4bbbba6558873beb43ec00b4b4a9df9c1bce9c
mklucifer <61414632+mklucifer@users.noreply.github.com>
2025-07-10 11:44:46 +0100

    Update debugging.lua

--- config/nvim/lua/plugins/debugging.lua
+++ config/nvim/lua/plugins/debugging.lua
@@ -1,5 +1,9 @@
 return {
-  "mfussenegger/nvim-dap"
+  "rcarriga/nvim-dap-ui",
+  dependencies = {
+    "mfussenegger/nvim-dap",
+    "nvim-neotest/nvim-nio"
+  },
   config = function()
     local dap = require("dap)
     vim.keymap.set('n', '<Leader>db', dap.toggle_breakpoint() {})
