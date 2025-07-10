107c6ee14cb103088d61b2f71be18266b3dbdc7b
mklucifer <61414632+mklucifer@users.noreply.github.com>
2025-07-10 12:57:10 +0100

    Update keymaps.lua

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -5,3 +5,11 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>n", ":bn<cr>")
 vim.keymap.set("n", "<leader>p", ":bp<cr>")
 vim.keymap.set("n", "<leader>x", ":bd<cr>")
+
+-- LSP
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
