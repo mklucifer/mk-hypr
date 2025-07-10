9e567584974dbc981c04923232a9c417fed939ce
mklucifer <61414632+mklucifer@users.noreply.github.com>
2025-07-10 14:30:40 +0100

    Update keymaps.lua

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -1,7 +1,15 @@
 vim.g.mapleader = " "
+
+-- buffers
 vim.keymap.set("n", "<leader>n", ":bn<cr>")
 vim.keymap.set("n", "<leader>p", ":bp<cr>")
 vim.keymap.set("n", "<leader>x", ":bd<cr>")
 
+-- Load up file in Markdown view
 vim.keymap.set("n", "<leader>mp", ":MarkdownPreviewToggle<cr>")
+
+-- Toggle comments on or off
 vim.keymap.set({"n", "v"}, "<leader>/", ":CommentToggle<cr>")
+
+-- Copy to clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
